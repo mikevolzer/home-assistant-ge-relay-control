5662b325d319b52929d58fe6a6146c254d117db8
Mike Volzer <mikevolzer@gmail.com>
2025-12-21 16:50:59 -0500

    Add abstract class for testing

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import paho.mqtt.client as mqtt
 import time
 import json
 import serial
+from abc import ABC, abstractmethod
 
 
 RELAY_DELAY_TIME_MS = 100.0
@@ -13,32 +14,62 @@ ROOT_TOPIC = "homeassistant/light"
 AVAILABLE_TOPIC = f"{ROOT_TOPIC}/available"
 
 
+class ArduinoInterface(ABC):
+    @abstractmethod
+    def send_command(self, pin: int):
+        pass
+
+    def close(self):
+        pass
+
+
+class SerialArduino(ArduinoInterface):
+    def __init__(self, port: str, baudrate: int):
+        self.serial = serial.Serial(port, baudrate, timeout=0.1)
+        time.sleep(0.1)
+        if self.serial.isOpen():
+            print(f'{self.serial.port} connected!')
+
+    def send_command(self, pin: int):
+        self.serial.write(f'<{pin}>'.encode())
+        time.sleep(0.2)
+
+    def close(self):
+        self.serial.close()
+
+
+class MockArduino(ArduinoInterface):
+    def send_command(self, pin: int):
+        print(f"Mock: Sending command to pin {pin}")
+
+
 class Relay:
-    def __init__(self, name: str, description: str, area: str, gpio_on: int, gpio_off: int):
+    def __init__(self, name: str, description: str, area: str, gpio_on: int, gpio_off: int, arduino_interface: ArduinoInterface):
         self.name = name
         self.description = description
         self.area = area
         self.gpio_on = gpio_on
         self.gpio_off = gpio_off
+        self.arduino_interface = arduino_interface
 
     def set_state(self, state: bool):
         pin_to_pulse = self.gpio_on if state else self.gpio_off
-        arduino.write(f'<{pin_to_pulse}>'.encode())
-        time.sleep(0.2)
-
-
-arduino = serial.Serial("/dev/ttyACM0", 115200, timeout=0.1)
-time.sleep(0.1)
-if arduino.isOpen():
-    print(f'{arduino.port} connected!')
+        self.arduino_interface.send_command(pin_to_pulse)
 
 relays = {}
+arduino_interface = None
 with open('config.json', 'r') as f:
     config = json.load(f)
     RELAY_DELAY_TIME_MS = config["delay_time_ms"]
     BROKER_HOST = config["broker"]
     USERNAME = config["username"]
     PASSWORD = config["password"]
+
+    
+    try:
+        arduino_interface = SerialArduino("/dev/ttyACM0", 115200)
+    except serial.SerialException:
+        arduino_interface = MockArduino()
 
     for relay in config["relays"]:
         try:
@@ -47,7 +78,7 @@ with open('config.json', 'r') as f:
             area = ""
 
         relays[f"relay_{relay['id']}"] = Relay(
-            relay["id"], relay["description"], area, relay["gpio_on"], relay["gpio_off"])
+            relay["id"], relay["description"], area, relay["gpio_on"], relay["gpio_off"], arduino_interface)
 
 
 def publish_homeassistant_config_info(client: mqtt.Client):
@@ -69,8 +100,10 @@ def publish_homeassistant_config_info(client: mqtt.Client):
 
 
 def on_connect(client: mqtt.Client, userdata, flags, reason_code, properties):
-    # if not reason_code.is_failure:
-    print("Connection to MQTT broker succeeded")
+    if reason_code.is_failure:
+        print(f"Failed to connect to MQTT broker: {reason_code}")
+        return
+    print(f"Connected to MQTT broker: {reason_code}")
     publish_homeassistant_config_info(client)
 
     client.publish(f"{ROOT_TOPIC}/available", "online", 1, True)
@@ -106,4 +139,4 @@ if __name__ == '__main__':
     except (KeyboardInterrupt, OSError):
         client.publish(AVAILABLE_TOPIC, "offline")
         client.disconnect()
-        arduino.close()
+        arduino_interface.close()
